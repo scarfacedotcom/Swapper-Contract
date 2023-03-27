42892de6005eefb59f9fc61db95fb5fea6cdba24
scarfacedotcom <peteradaaku@gmail.com>
2023-03-27 19:18:24 +0100

    scripting

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,26 @@
 import { ethers } from "hardhat";
 
+const ConstructionArgs = {
+  // DAI TOKEN
+    token1: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    // ETH TOKEN
+    token2: "0xFfb99f4A02712C909d8F7cC44e67C87Ea1E71E83",
+    // DAI/USD PRICEFEED
+    priceFeed1: "0x0d79df66BE487753B02D015Fb622DED7f0E9798d",
+    priceFeed2: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e"
+}
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  //const lockedAmount = ethers.utils.parseEther("1");
 
-  await lock.deployed();
+  const TokenSwap = await ethers.getContractFactory("TokenSwap");
+  const tokenSwap = await TokenSwap.deploy(ConstructionArgs.token1, ConstructionArgs.token2, ConstructionArgs.priceFeed1, ConstructionArgs.priceFeed2);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await tokenSwap.deployed();
+
+  console.log(`Token swap contract has been deployed to ${tokenSwap.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
